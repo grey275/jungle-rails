8df8a51cba14a2829c0fd376b3d2dfef032b93b9
grey275 <pjvanderpol@gmail.com>
2019-06-17 11:38:24 -0700

    add validation testing for product

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      elves = Category.new(name: 'elves')
+      product = Product.new(name: 'Elf on a shelf', price: 10, quantity: 3, category: elves)
+    end
+    it 'not valid without name' do
+      product = Product.new(name: nil)
+      expect(product).to_not be_valid
+    end
+    it 'not valid without price' do
+      product = Product.new(price: nil)
+      expect(product).to_not be_valid
+    end
+    it 'not valid without quantity' do
+      product = Product.new(quantity: nil)
+      expect(product).to_not be_valid
+    end
+    it 'not valid without category' do
+      product = Product.new(category: nil)
+      expect(product).to_not be_valid
+    end
+  end
+end
