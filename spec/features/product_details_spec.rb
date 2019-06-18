53cbaa5699e11b10ce90beffc60d9d25f43db590
grey275 <pjvanderpol@gmail.com>
2019-06-18 13:12:25 -0700

    add link navigation test

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "ProductDetails", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
@@ -16,16 +16,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
-    # ACT
+  scenario "They click on a details button for a product" do
+    # act
     visit root_path
-
-    # DEBUG / VERIFY
     first('.product').click_link('Details')
+
+    visit current_url
     save_screenshot
 
-    # condition
-    expect(page).to have_css 'article.product', count: 10
+    # debug/verify
+    expect(current_path).to match /\/products\/\d/
   end
-
 end
